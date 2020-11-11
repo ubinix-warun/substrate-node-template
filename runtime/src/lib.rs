13646ced5b332110e9cfc37146741dc387811297
ubinix-warun <warun@ubinix.com>
2020-11-11 18:03:59 +0700

    ADD: TunaLedger

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -40,6 +40,7 @@ pub use frame_support::{
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_tuna;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -266,6 +267,10 @@ impl pallet_template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl pallet_tuna::Trait for Runtime {
+	type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +288,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		TunaLedger: pallet_tuna::{Module, Call, Storage, Event<T>},
 	}
 );
 
